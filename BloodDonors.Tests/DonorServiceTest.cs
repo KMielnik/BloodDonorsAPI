142ca88683bc26b25fda4b45e02032eb48577aa3
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-07-24 00:13:32 +0200

    Add Tests test

---
+++ BloodDonors.Tests/DonorServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BloodDonors.Tests
+{
+    [TestClass]
+    public class DonorServiceTest
+    {
+        [TestMethod]
+        public async Task test()
+        {
+            Assert.AreEqual(true,true);
+        }
+    }
+}
