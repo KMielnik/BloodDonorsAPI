ae0329d1cf6545aaeb6793971ae113db9efaf69d
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-07-23 21:15:27 +0200

    Add BloodType Repo

---
+++ BloodDonors.Core/Repositories/IBloodTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BloodDonors.Core.Domain;
+
+namespace BloodDonors.Core.Repositories
+{
+    public interface IBloodTypeRepository
+    {
+        Task<IEnumerable<BloodType>> GetAllAsync();
+    }
+}
---
+++ BloodDonors.Infrastructure/Repositories/InMemoryBloodTypeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BloodDonors.Core.Repositories;
+using BloodDonors.Core.Domain;
+
+namespace BloodDonors.Infrastructure.Repositories
+{
+    public class InMemoryBloodTypeRepository : IBloodTypeRepository
+    {
+        private static readonly ISet<BloodType> bloodTypes = new HashSet<BloodType>
+        {
+            new BloodType("A", '+'),
+            new BloodType("B", '-'),
+            new BloodType("AB", '-'),
+            new BloodType("O", '-'),
+            new BloodType("A", '+'),
+            new BloodType("B", '+'),
+            new BloodType("AB", '+'),
+            new BloodType("O", '+')
+        };
+
+        public async Task<IEnumerable<BloodType>> GetAllAsync()
+            => await Task.FromResult(bloodTypes);
+    }
+}
