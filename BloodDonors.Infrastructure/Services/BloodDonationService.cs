3c298c8cd9651f0cbe6ded763e5ef52827de7ef6
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-07-24 10:13:30 +0200

    fix

--- BloodDonors.Infrastructure/Services/BloodDonationService.cs
+++ BloodDonors.Infrastructure/Services/BloodDonationService.cs
@@ -1,7 +1,19 @@
-﻿namespace BloodDonors.Infrastructure.Services
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using BloodDonors.Infrastructure.DTO;
+
+namespace BloodDonors.Infrastructure.Services
 {
     public class BloodDonationService : IBloodDonationService
     {
-        
+        public async Task<IEnumerable<BloodDonationDTO>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<int> HowMuchBloodHasBeenDonatedEver()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
