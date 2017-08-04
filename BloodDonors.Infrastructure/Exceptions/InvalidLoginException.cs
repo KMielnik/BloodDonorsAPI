a7e419c9f9c7485f4bb09c4fe162fdaf0ad450b6
Kamil Mielnik <kamil.mielnik.km@gmail.com>
2017-08-04 14:47:40 +0200

    Added custom exceptions.

---
+++ BloodDonors.Infrastructure/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BloodDonors.Infrastructure.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public InvalidLoginException()
+        {
+        }
+
+        public InvalidLoginException(string message) : base(message)
+        {
+        }
+
+        public InvalidLoginException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
